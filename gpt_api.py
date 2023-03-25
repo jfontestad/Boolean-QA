87fc0291aa97dc47126276113b11f352cb02bbc9
shreayan98c <shreayan98c@gmail.com>
2023-03-25 02:39:34 -0400

    Evaluating GPT API few shot performance

--- gpt_api.py
+++ gpt_api.py
@@ -8,25 +8,14 @@ from datasets import load_dataset
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-# response = openai.Completion.create(
-#   model="davinci-instruct-beta",
-#   prompt="Correct this English text: Today I have went to the store to to buys some many bottle of water.\n\nToday I have gone to the store to buy some water bottles.",
-#   temperature=0.7,
-#   max_tokens=256,
-#   top_p=1,
-#   frequency_penalty=0,
-#   presence_penalty=0
-# )
-
-
-def pre_process(num_shots=8):
+def pre_process(num_shots=8, eval_records=30):
     # download dataset
     print("Loading the dataset ...")
     dataset = load_dataset("boolq")
     dataset = dataset.shuffle()  # shuffle the data
 
     # random sampling num_shots / 2 true and num_shots / 2 false prompts
-    print(f"Randomly selecting {num_shots} prompts from dataset...")
+    print(f"Randomly selecting {num_shots} prompts from dataset ...")
     true_samples, ct_true = [], 0
     false_samples, ct_false = [], 0
     rand_idx = np.random.randint(0, len(dataset['train']), num_shots * 2)
@@ -43,7 +32,12 @@ def pre_process(num_shots=8):
     samples = true_samples + false_samples
     random.shuffle(samples)
 
-    return samples
+    # random sampling eval_records prompts from the validation data
+    print(f"Randomly selecting {eval_records} prompts from validation dataset ...")
+    rand_idx = np.random.randint(0, len(dataset['validation']), eval_records)
+    test_samples = [dataset['validation'][int(idx)] for idx in rand_idx]
+
+    return samples, test_samples
 
 
 def create_prompt_str(samples):
@@ -57,8 +51,46 @@ def create_prompt_str(samples):
         prompt += "\n"
         prompt += "Answer: "
         prompt += "True" if samp['answer'] else "False"
-        prompt += "\n\n"
+        prompt += "\n"
     return prompt
+
+
+def evaluate_gpt3(prompt, test_samples):
+    correct_preds = 0
+    total_preds = len(test_samples)
+    for test_sample in test_samples:
+        prompt_curr_sample = prompt
+        prompt_curr_sample += "Passage: "
+        prompt_curr_sample += test_sample['passage']
+        prompt_curr_sample += "\n"
+        prompt_curr_sample += "Question: "
+        prompt_curr_sample += test_sample['question'] + "?"
+        prompt_curr_sample += "\n"
+        prompt_curr_sample += "Answer:"
+
+        # call the OpenAI GPT API on the prompt along with the current to pred the ans
+        response = openai.Completion.create(
+            model="davinci",
+            prompt=prompt_curr_sample,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+
+        gpt_ans = response['choices'][0]['text']
+        # remove extra prompts from the gpt's response - end when \n is encountered
+        if '\n' in gpt_ans:
+            gpt_ans = gpt_ans[:gpt_ans.index('\n')]
+
+        gpt_pred = 'true' in gpt_ans.lower()
+        print(f"Predicted: {gpt_pred}, Actual: {test_sample['answer']}")
+        if gpt_pred == test_sample['answer']:
+            correct_preds += 1
+
+    print(f'Correct Preds: {correct_preds}, Total Preds: {total_preds}')
+    return correct_preds / total_preds * 100
 
 
 # the entry point of the program
@@ -69,9 +101,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(f"Specified arguments: {args}")
 
-    # load the data and models
-    samples = pre_process(num_shots=8)
-    prompt = create_prompt_str(samples)
-    print(prompt)
+    # load the data and preprocess for prompts and eval data
+    samples, test_samples = pre_process(num_shots=8, eval_records=30)
+    prompt = create_prompt_str(samples=samples)
 
-    print(" >>>>>>>>  Sending prompt to GPT-3 API ... ")
+    print(" >>>>>>>>  Sending prompts to GPT-3 API ... ")
+    accuracy = evaluate_gpt3(prompt=prompt, test_samples=test_samples)
+    print(f'Accuracy: {accuracy}%')
